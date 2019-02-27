127e409bcdaa68ca183df241307aadd40d7f6f8b
wandersick <wandersick@gmail.com>
2019-02-27 09:54:08 +0800

    Add currentDateTime

--- Scripts/ws-dynamic-group-core.ps1
+++ Scripts/ws-dynamic-group-core.ps1
@@ -1,11 +1,21 @@
-# c:\ws-dynamic-group
+# Script Directory
+# Example: c:\ws-dynamic-group
 $scriptDir = "c:\ws-dynamic-group"
-# /add /del
+
+# Action to perform on usernames to groups
+# Example: /add /del
 $action = "/add"
 
-$users = import-csv "$ScriptDir\01_Incoming\incoming.csv"
+# Create a new directory by randomizing a unique value made up of day time.
+# Example: 20190227_095047AM
+$currentDateTime = Get-Date -format "yyyyMMdd_hhmmsstt"
+
+# Backup existing group members to a log file
+
+<# $users = import-csv "$ScriptDir\01_Incoming\incoming.csv"
 ForEach ($user in $users){
-$username = $($users.username)
-$groupname = $($users.groupname)
-net localgroup `"$groupname`" `"$username`" $action
-}
+    $username = $($users.username)
+    $groupname = $($users.groupname)
+    net localgroup `"$groupname`" `"$username`" $action
+} #>
+
