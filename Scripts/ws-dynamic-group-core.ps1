eca72f0369f681ff94ff3fb8af881981af73e609
ningng <ningng@outlook.com>
2019-02-27 10:26:33 +0800

    Backup existing group members to a log file (Output File: ExistingGroupMemberBackup_GroupName.csv)

--- Scripts/ws-dynamic-group-core.ps1
+++ Scripts/ws-dynamic-group-core.ps1
@@ -5,9 +5,13 @@ $scriptDir = "c:\ws-dynamic-group"
 # CSV filename to process
 $csvFile = "incoming.csv"
 
-# Action to perform on usernames to groups
+# Action to perform on users and groups
 # Example: /add /del
 $action = "/add"
+
+# Remove existing users from group (only effective when action="/add")
+# Example: $true
+$removeExUsersFromGroup = $true
 
 # Create a new directory by randomizing a unique value made up of day time.
 # Example: 20190227_095047AM
@@ -18,12 +22,12 @@ New-Item "$scriptDir\02_Processing\$currentDateTime" -Force -ItemType "directory
 Copy-Item "$scriptDir\01_Incoming\$csvFile" "$scriptDir\02_Processing\$currentDateTime\$csvFile" -Force
 Remove-Item "$scriptDir\01_Incoming\$csvFile" -force 
 
-# Backup existing group members to a log file
-
-$users = import-csv "$ScriptDir\01_Incoming\incoming.csv"
+$users = import-csv "$scriptDir\02_Processing\$currentDateTime\$csvFile"
 ForEach ($user in $users){
     $username = $($users.username)
     $groupname = $($users.groupname)
-    net localgroup `"$groupname`" `"$username`" $action
+    # Backup existing group members to a log file (Output File: ExistingGroupMemberBackup_GroupName.csv)
+    Get-LocalGroup "$groupname" | Get-LocalGroupMember | Export-CSV "$scriptDir\02_Processing\$currentDateTime\ExistingGroupMemberBackup_$groupname.csv"
+    # Perform action on user and group
+    <# net localgroup `"$groupname`" `"$username`" $action #>
 }
-
