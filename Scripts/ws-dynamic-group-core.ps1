5785db99f71020d4a11f64cfce144255a8bbdbb8
ningng <ningng@outlook.com>
2019-02-27 10:10:55 +0800

    Move 01_Incoming\incoming.csv to a directory of ransomized name inside 02_Processing

--- Scripts/ws-dynamic-group-core.ps1
+++ Scripts/ws-dynamic-group-core.ps1
@@ -1,6 +1,9 @@
-# Script Directory
+# Script directory
 # Example: c:\ws-dynamic-group
 $scriptDir = "c:\ws-dynamic-group"
+
+# CSV filename to process
+$csvFile = "incoming.csv"
 
 # Action to perform on usernames to groups
 # Example: /add /del
@@ -10,12 +13,17 @@ $action = "/add"
 # Example: 20190227_095047AM
 $currentDateTime = Get-Date -format "yyyyMMdd_hhmmsstt"
 
+# Move 01_Incoming\incoming.csv to a directory of ransomized name inside 02_Processing
+New-Item "$scriptDir\02_Processing\$currentDateTime" -Force -ItemType "directory"
+Copy-Item "$scriptDir\01_Incoming\$csvFile" "$scriptDir\02_Processing\$currentDateTime\$csvFile" -Force
+Remove-Item "$scriptDir\01_Incoming\$csvFile" -force 
+
 # Backup existing group members to a log file
 
-<# $users = import-csv "$ScriptDir\01_Incoming\incoming.csv"
+$users = import-csv "$ScriptDir\01_Incoming\incoming.csv"
 ForEach ($user in $users){
     $username = $($users.username)
     $groupname = $($users.groupname)
     net localgroup `"$groupname`" `"$username`" $action
-} #>
+}
 
